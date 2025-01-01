55a682e524297061b1490a71cfb867007bd21e27
Ali Alkhaled <78077817+alialk-s@users.noreply.github.com>
2025-01-01 21:42:49 +0100

    Distribute worker impl

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -97,95 +97,64 @@ func executeMapTask(task ExampleReply, mapf func(string, string) []KeyValue) {
 }
 
 func executeReduceTask(task ExampleReply, reducef func(string, []string) string) {
-	intermediate := []KeyValue{}
-	for i := 0; i < task.NMap; i++ {
-		filename := fmt.Sprintf("mr-%d-%d.json", i, task.ReduceNumber)
-		file, err := os.Open(filename)
-		if err != nil {
-			log.Printf("Cannot open %v. Reporting failure.", filename)
-			reportFailureToCoordinator(task, "reduce", err.Error())
-			return
-		}
-		dec := json.NewDecoder(file)
-		for {
-			var kv KeyValue
-			if err := dec.Decode(&kv); err != nil {
-				break
-			}
-			intermediate = append(intermediate, kv)
-		}
-		file.Close()
-	}
+    intermediate := []KeyValue{}
+    for i := 0; i < task.NMap; i++ {
+        filename := fmt.Sprintf("mr-%d-%d.json", i, task.ReduceNumber)
+        file, err := os.Open(filename)
+        if err != nil {
+            log.Printf("Cannot open %v. Reporting failure.", filename)
+            reportFailureToCoordinator(task, "reduce", err.Error())
+            return
+        }
+        dec := json.NewDecoder(file)
+        for {
+            var kv KeyValue
+            if err := dec.Decode(&kv); err != nil {
+                break
+            }
+            intermediate = append(intermediate, kv)
+        }
+        file.Close()
+    }
 
-	sort.Slice(intermediate, func(i, j int) bool {
-		return intermediate[i].Key < intermediate[j].Key
-	})
+    sort.Slice(intermediate, func(i, j int) bool {
+        return intermediate[i].Key < intermediate[j].Key
+    })
 
-	outputFile := fmt.Sprintf("mr-out-%d.json", task.ReduceNumber)
-	out, err := os.Create(outputFile)
-	if err != nil {
-		log.Fatalf("Cannot create %v", outputFile)
-	}
-	defer out.Close()
+    output := make(map[string]string)
+    i := 0
+    for i < len(intermediate) {
+        j := i + 1
+        for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+            j++
+        }
+        values := []string{}
+        for k := i; k < j; k++ {
+            values = append(values, intermediate[k].Value)
+        }
+        output[intermediate[i].Key] = reducef(intermediate[i].Key, values)
+        i = j
+    }
 
-	i := 0
-	for i < len(intermediate) {
-		j := i + 1
-		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
-			j++
-		}
-		values := []string{}
-		for k := i; k < j; k++ {
-			values = append(values, intermediate[k].Value)
-		}
-		output := reducef(intermediate[i].Key, values)
-		fmt.Fprintf(out, "%v %v\n", intermediate[i].Key, output)
-		i = j
-	}
+    call("Coordinator.SaveReduceOutput", &ReduceOutputArgs{
+        ReduceNumber: task.ReduceNumber,
+        Output:       output,
+    }, nil)
 
-	call("Coordinator.ReportCompletion", &ReportCompletionArgs{
-		TaskType:     "reduce",
-		MapNumber:    -1,
-		ReduceNumber: task.ReduceNumber,
-	}, nil)
+    call("Coordinator.ReportCompletion", &ReportCompletionArgs{
+        TaskType:     "reduce",
+        ReduceNumber: task.ReduceNumber,
+        MapNumber:    -1,
+    }, nil)
 }
-
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-/*
-
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
-}*/
 
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
-	c, err := rpc.DialHTTP("unix", sockname)
+	c, err := rpc.DialHTTP("tcp", "10.214.0.2"+":1234")
+	//sockname := coordinatorSock()
+	//c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
